097ef7c37bc4854d17e20898326280f422c0363f
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2023-11-10 08:29:05 -0500

    Reorder elements for UX

--- src/header.js
+++ src/header.js
@@ -7,12 +7,12 @@ export { loadHeader }
 function loadHeader() {
     const header = document.querySelector('header');
     header.innerHTML = `
-        <img src=${logo} alt="Application logo" id="logo">
+        <div id="logo"><img src=${logo} alt="Application logo"></div>
+        <div id="add-btn">+</div>
         <div id="next-due">
             <img src=${clock} alt="Icon of a clock" id="clock">
             <span>Nov 11</span>
         </div>
-        <div id="add-btn">+</div>
         <img src=${profilePic} alt="Profile photo" id="profile">
     `;
 }
