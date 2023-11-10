f53954743981d835b5873d2755389c09b1039c71
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2023-11-10 10:52:03 -0500

    Add sidebar loading function call

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './style.css';
 import { loadHeader } from "./header";
 import { loadFooter } from './footer';
+import { loadSidebar } from './sidebar';
 
 loadHeader();
 loadFooter();
+loadSidebar();
