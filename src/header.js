e051376f5a6fa7f4c6c80ab0397bff7e2a62147b
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2023-11-10 09:05:43 -0500

    Change add button id to a class

--- src/header.js
+++ src/header.js
@@ -8,7 +8,7 @@ function loadHeader() {
     const header = document.querySelector('header');
     header.innerHTML = `
         <div id="logo"><img src=${logo} alt="Application logo"></div>
-        <div id="add-btn">+</div>
+        <div class="add-btn">+</div>
         <div id="next-due">
             <img src=${clock} alt="Icon of a clock" id="clock">
             <span>Nov 11</span>
