3d75e9255ada456a5936380df5acaeaf02275591
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2023-11-13 16:12:14 -0500

    Add import for Task module

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import './style.css';
 import { loadHeader } from "./header";
 import { loadFooter } from './footer';
 import { loadSidebar } from './sidebar';
+import { Task } from './task';
 
 loadHeader();
 loadFooter();
 loadSidebar();
+
+if (Object.keys(Task.tasks).length === 0) {
+    let task1 = Task.addTask("hi","my","name","is","brett", false);
+    let task2 = Task.addTask("hi","my","name","is","berscilla", false);
+    let task3 = Task.addTask("hi","my","name","is","gus", false);
+}
+console.log(Task.tasks)
+console.log(Task.currentId)
