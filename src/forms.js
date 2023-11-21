a2001915dfd341fd3feb1374d62c15ebcff56348
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2023-11-21 14:50:59 -0500

    Add module for generating form elements

---
+++ src/forms.js
@@ -0,0 +1,17 @@
+// Creates form DOM elements for the page
+
+const Forms = (function() {
+    
+    // Form for adding tasks
+    function getAddTaskForm() {
+        
+    }
+
+    // Form for adding projects
+    function getAddProjectForm() {
+
+    }
+
+    return { getAddTaskForm, getAddProjectForm }
+
+})();
