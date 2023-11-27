4cc87471c92362e0c472e8403311e241c93f621a
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2023-11-27 09:00:11 -0500

    Add project list to sidebar

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,4 +1,5 @@
 import { UiControl } from "./uiControl";
+import { Project } from "./project";
 export { loadSidebar }
 
 
@@ -13,13 +14,13 @@ function loadSidebar() {
             <li class="filter done" tabindex="0">Done</li>
         </ul>
         <div class="project-explorer">
-            <div class="title">
-                <h3>Projects</h3>
-                <div class="add-project-btn" tabindex="0">+</div>
-            </div>
+            <h3 class="title">Projects</h3>
             <ul class="projects"></ul>
         </div>
     `;
+
+    // Add projects
+    addProjectList();
 
     const allBtn = sidebar.querySelector(".filter.all");
     allBtn.addEventListener('click', UiControl.displayAllTasks);
@@ -35,4 +36,79 @@ function loadSidebar() {
 
     const doneBtn = sidebar.querySelector(".filter.done");
     doneBtn.addEventListener("click", UiControl.displayCompletedTasks)
+
+    // const addProjectButton = sidebar.querySelector(".add-project-btn");
+    // addProjectButton.addEventListener('click', (event) => UiControl.displayAddProjectForm(event));
+}
+
+// Function to add all saved projects ot the project list
+function addProjectList() {
+    // Get projects
+    const projectNames = Project.getAllProjectNames();
+
+    // Create sidebar elements
+    const projectsListEl = document.querySelector(".sidebar .projects");
+    projectNames.forEach(projectName => addProjectToList(projectName));
+
+    // Add the element that adds a project
+    addAddProjectElement();
+}
+
+// Function to append a single project to the end of the project list
+function addProjectToList(projectName) {
+    const projectsListEl = document.querySelector(".sidebar .projects");
+    const projectEl = document.createElement("li");
+    projectEl.classList.add("filter", "project");
+    projectEl.setAttribute('tabindex', '0');
+    projectEl.innerText = projectName;
+    projectEl.addEventListener('click', (event) => UiControl.displayProject(event));
+    projectsListEl.appendChild(projectEl);
+}
+
+// Function to add the "add project button" to the end of the project list
+function addAddProjectElement() {
+    const projectsListEl = document.querySelector(".sidebar .projects");
+    const addProjectEl = document.createElement("li");
+    addProjectEl.classList.add("add-project-btn", "project", "filter");  // add filter class to match formatting
+    addProjectEl.setAttribute('tabindex', '0');
+    addProjectEl.innerText = 'New Project +';
+    addProjectEl.addEventListener('click', (event) => addProjectPressed(event));
+    projectsListEl.appendChild(addProjectEl);
+}
+
+// Callback for when add project button is pressed
+function addProjectPressed(event) {
+    // Remove the project adding button 
+    const addProjectEl = event.target;
+    const parent = addProjectEl.parentNode;
+    addProjectEl.remove();
+
+    // Create an input element
+    const newProjectInput = document.createElement("form");
+    newProjectInput.classList.add("add-project-form");
+    newProjectInput.innerHTML = `
+        <input type="text" name="project-name" id="project-input" placeholder="Project Name" autofocus="autofocus" required></input>
+    `;
+
+    // Attach event listener for form submission
+    newProjectInput.addEventListener("submit", event => newProjectSubmitted(event));
+
+    // Place it where the add project button was
+    parent.appendChild(newProjectInput);
+
+}
+
+// Callback for when a new project name is submitted
+function newProjectSubmitted(event) {
+    event.preventDefault();
+    // Add to list of projects
+    const input = document.querySelector('#project-input');
+    const projectName = input.value;
+    Project.addNewProject(projectName);
+    // Remove the form from th DOM
+    document.querySelector(".add-project-form").remove();
+    // Add to sidebar list
+    addProjectToList(projectName);
+    // Add add project button back to sidebar list
+    addAddProjectElement();
 }
