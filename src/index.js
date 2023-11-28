22bf437a9c3b15339efd2a07f919388ddc8997f2
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2023-11-28 14:34:00 -0500

    Finish main control flow

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import './style.css';
-import { loadHeader } from "./header";
+import { UiControl } from './uiControl';
+import { Forms } from './forms';
+import { Header } from "./header";
 import { loadFooter } from './footer';
 import { loadSidebar } from './sidebar';
-import { Task } from './task';
 
-loadHeader();
+Header.loadHeader();
 loadFooter();
 loadSidebar();
-
-if (Object.keys(Task.tasks).length === 0) {
-    let task1 = Task.addTask("hi","my","name","is","brett", false);
-    let task2 = Task.addTask("hi","my","name","is","berscilla", false);
-    let task3 = Task.addTask("hi","my","name","is","gus", false);
-}
-console.log(Task.tasks)
-console.log(Task.currentId)
+Forms.addHiddenForm();
+UiControl.displayAllTasks();
