3a483a4b0a9ac887b9d3ad8243ee20de03e4a1d4
Brett <127159734+sourdoughbredd@users.noreply.github.com>
2023-12-01 09:23:15 -0500

    Finalize startup sequence

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import { UiControl } from './uiControl';
 import { Forms } from './forms';
 import { Header } from "./header";
 import { loadFooter } from './footer';
-import { loadSidebar } from './sidebar';
+import { Sidebar } from './sidebar';
 
 Header.loadHeader();
 loadFooter();
-loadSidebar();
+Sidebar.loadSidebar();
 Forms.addHiddenForms();
 UiControl.displayAllTasks();
